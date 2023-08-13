a7caedad58e802e668bd5672012c3584950189c5
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2023-08-13 11:30:42 -0500

    Primer commit

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+
+import Paciente from "./Paciente";
+
+export const ListadoPacientes = ({
+  pacientes,
+  setPaciente,
+  eliminarPaciente,
+}) => {
+  useEffect(() => {
+    if (pacientes.length > 0) {
+      console.log("nuevo paciente");
+    }
+  }, [pacientes]);
+
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-lg mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">pacientes y citas</span>
+          </p>
+
+          {pacientes.map((paciente) => (
+            <Paciente
+              key={paciente.id}
+              paciente={paciente}
+              setPaciente={setPaciente}
+              eliminarPaciente={eliminarPaciente}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-lg mt-5 mb-10 text-center">
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold">
+              y aparecerán en este lugar
+            </span>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
